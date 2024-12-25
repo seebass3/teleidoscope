4f8e9003fc4404354da8f52906a97811dcf680ba
Sebastien Berthiaume <sebastien.berth94@gmail.com>
2024-12-24 22:48:23 -0800

    Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,32 @@
-import typography from "@tailwindcss/typography";
-import type { Config } from "tailwindcss";
+import typography from '@tailwindcss/typography'
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-    },
-    extend: {
-      boxShadow: {
-        layer: "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
-      },
-      colors: {
-        offWhite: "#faf9f7",
-        slate: "#324e53",
-        sandstone: "#eeeae4",
-        moss: "#738889",
-        flint: "#1f2424",
-        solar: "#ff6d00"
-      },
-      fontFamily: {
-        sans: ["var(--font-inter)"],
-      },
-    },
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [typography],
-} satisfies Config;
+	content: ['./app/**/*.{ts,tsx}', './sanity/**/*.{ts,tsx}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+		extend: {
+			boxShadow: {
+				layer: '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+			},
+			colors: {
+				offWhite: '#faf9f7',
+				slate: '#324e53',
+				sandstone: '#eeeae4',
+				moss: '#738889',
+				flint: '#1f2424',
+				solar: '#ff6d00',
+			},
+			fontFamily: {
+				sans: ['var(--font-inter)'],
+			},
+		},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [typography],
+} satisfies Config
